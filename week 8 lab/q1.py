7b3c4bbeb92168b4477ea76b6ac71b75344ad4ce
patshirlow <patshirlow@gmail.com>
2026-04-04 17:28:27 +1100

    look how much more amazing this is

--- week 8 lab/q1.py
+++ week 8 lab/q1.py
@@ -3,6 +3,16 @@
 def div(a, b):
     return a / b
 
-result = div(10, 0)
+result = div(10, 5)
 print(result)
 
+# b)
+def safe_divide(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print("no")
+        return None
+
+print(safe_divide(10, 0))
+print(safe_divide(10, 5))
