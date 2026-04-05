ecc450e4f85822b62ab3312ad96b1c75b266468f
patshirlow <patshirlow@gmail.com>
2026-04-05 16:26:25 +1000

    did q1 gng

---
+++ week 9 lab/q1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+v0 = 5
+g = 9.81
+t = np.linspace(0, 1, 1001)
+y = v0 * t - 0.5 * g * t ** 2
+
+plt.plot(t, y, 'g--') # plot y versus t
+plt.ylabel('ball height, y (m)')
+plt.xlabel('time, t (s)')
+plt.grid(True)
+plt.title('ball height over time [0,1] seconds')
+plt.axis([0, 1, 0, 1.4])
+plt.show()
