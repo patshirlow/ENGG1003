3285db9b8b2c3f756ae0336ac76cb8d94c22c647
patshirlow <patshirlow@gmail.com>
2026-04-08 17:17:08 +1000

    completed question 2

--- week 9 lab/q2.py
+++ week 9 lab/q2.py
@@ -1,2 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+x = np.linspace(0, 1, 101)
+y = np.exp(x) - 3 * x
+
+plt.plot(x, y)
+plt.axis([0, 2, 0, 2])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Exponential')
+plt.show()
