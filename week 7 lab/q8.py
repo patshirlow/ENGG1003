7fea74f1f7603680952ffb870cf8ccd16093c137
patshirlow <patshirlow@gmail.com>
2026-04-10 10:19:12 +1000

    did q8 and q9 from week 7 lab

--- week 7 lab/q8.py
+++ week 7 lab/q8.py
@@ -1,1 +1,6 @@
 import numpy as np
+import numpy as np
+
+# 10 random integers from {0,1,2}
+x = np.random.randint(0, 3, size=10)
+print(x)
---
+++ week 7 lab/q9.py
@@ -0,0 +1,5 @@
+import numpy as np
+np.random.seed(501)
+
+x = np.random.randint(0, 2, size=10)
+print(x)
